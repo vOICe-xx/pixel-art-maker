9ce58305578f01d21b20d07c527b66e56bd3b488
vOICe <lvoicel@ya.ru>
2018-08-20 00:26:58 +0300

    Trying to add paiting

--- pixel-maker.js
+++ pixel-maker.js
@@ -3,7 +3,11 @@ const btnCreate = document.getElementById("Create");
 const GridinpH = document.getElementById("GridinpH");
 const GridinpW = document.getElementById("GridinpW");
 
+
 function gridCreate(height, width){
+/* 	document.getElementsByTagName('body')[0].addEventListener('keydown', console.log('body'));
+	pixWindow.addEventListener('keydown', console.log('pixWindow'));
+	document.getElementById('Main-block').addEventListener('keydown', console.log(event.which)); */
 	for (i = 1; i <= height; i++){
 		var pRow = document.createElement("div");
 		pixWindow.appendChild(pRow);
@@ -13,11 +17,29 @@ function gridCreate(height, width){
 			pRow.appendChild(pix);
 		}
 	}	
-}
-btnCreate.addEventListener("click", function(event){
+};
+var pixels = document.querySelectorAll(".Pixel");
+/* function draw(){
+	for (let j = 0; j < pixels.length; j++){
+		pixels[j].id = curColor;
+	};
+}; */
+for (let j = 0; j < pixels.length; j++){
+/* 	pixels[j].addEventListener('keydown', console.log('pix')); */
+	pixels[j].id = curColor;
+};
+btnCreate.addEventListener("click", function(){
 var height = GridinpH.value;
 var width = GridinpW.value;
 pixWindow.innerHTML = '';
 gridCreate(height, width);
-console.log(height);
 });
+var colors = document.querySelectorAll(".ColorPall");
+var curColor = '';
+for (let i = 0; i < colors.length; i++){
+  colors[i].addEventListener('click', function(){
+		var curColorBox = document.getElementsByClassName("CurColor");
+    curColor = this.id;
+		curColorBox[0].id = this.id;
+   });
+};
