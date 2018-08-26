550a9ccc02bfe5e8aaf48371fb48274386643be6
vOICe <lvoicel@ya.ru>
2018-08-27 00:25:46 +0300

    Added save/load buttons

--- pixel-maker.js
+++ pixel-maker.js
@@ -2,8 +2,37 @@ const pixWindow = document.getElementById("Pixel-maker");
 const btnCreate = document.getElementById("Create");
 const GridinpH = document.getElementById("GridinpH");
 const GridinpW = document.getElementById("GridinpW");
+const btnFill = document.getElementById('Fill');
+const btnBrush = document.getElementById('Brush');
+const colorPicker = document.getElementById('Color');
+const colorWrap = document.getElementById('ColorWrap');
+const btnLoad = document.getElementById('Load');
+const btnSave = document.getElementById('Save');
+colorWrap.style.background = colorPicker.value;
+var pixels = null;
+var curColor = 'black';
+var curTool = 1;
+var isDown = null;
+var curPixel = null;
+var mouBtn = null;
+var saveFile = {
+	'pixels': []
+};
+var loadFile = {
+	'pixels': []
+};
 
-function gridCreate(height, width){ //Создание канваса
+colorPicker.addEventListener('change', function(){
+	curColor = event.target.value;
+	colorWrap.style.background = curColor;
+});
+
+
+var gridCreate = function gridCreate(height, width){ //Создание канваса
+pixWindow.innerHTML = '';
+pixels = null;
+var height = GridinpH.value;
+var width = GridinpW.value;
 	for (i = 1; i <= height; i++){ 
 		var pRow = document.createElement("div");
 		pixWindow.appendChild(pRow); //Добавление строки 
@@ -13,60 +42,78 @@ function gridCreate(height, width){ //Создание канваса
 			pRow.appendChild(pix); //Добавление ячейки
 		}
 	}
+pixels = document.querySelectorAll(".Pixel"); //Сбор всех пикселей в канвасе
 };
 
-btnCreate.addEventListener("click", function(){ //Кнопка создания канваса
-var height = GridinpH.value;
-var width = GridinpW.value;
-pixWindow.innerHTML = '';
-gridCreate(height, width);
-});
+gridCreate();
 
-pixWindow.addEventListener('mouseenter', function brush(){
-	var pixels = document.querySelectorAll(".Pixel"); //Сбор всех пикселей в канвасе
+var switcher = function switcher(){
+	if (curTool == 0){
+		fill();
+		console.log(curTool);
+	} else {
+		brush();
+		console.log(curTool);
+	};
+};
+
+var fill = function fill(){
+	curTool = 1;
+	for (let i = 0; i < pixels.length; i++){
+		pixels[i].addEventListener('click', function(){
+			var toFill = [];
+			var firstToFill = i;
+			var sameColor = pixels[i].style.background;
+				console.log(sameColor);
+				
+		});
+	};
+};
+
+btnCreate.addEventListener("click", gridCreate);
+
+var brush = function brush(){
+	curTool = 0;
+	pixWindow.addEventListener('mousedown', function(){ 
+		isDown = true; 
+		mouBtn = event.which;
+	});
+	pixWindow.addEventListener('mouseup', function(){ //Выключаем рисование
+		isDown = false;
+	});
 	for (let j = 0; j < pixels.length; j++){
-		var isDown = false;
-		pixels[j].addEventListener('mouseover', function(){ //Вход в пиксель
-			var curPixel = event.target;
-			pixels[j].addEventListener('mousedown', function(){ //Меняем цвет при нажатии кнопки
-				isDown = true;
-				if (event.which == 3){
-					curPixel.style.background = 'white';
-				}
-				else {
-					curPixel.style.background = curColor;
-				};
-			});
-			pixels[j].addEventListener('mouseup', function(){ //Выключаем рисование
-				isDown = false;
-			});
+			pixels[j].addEventListener('mouseenter', function(){
+			curPixel = event.target;
 			if (isDown){
 				curPixel.style.background = curColor; //Меняем цвет при нажатой кнопке
+				if (mouBtn == 3){
+				curPixel.style.background = 'white';
+				};
 			};
-		}); 
- 	}; 
+		});
+	};
+}; 
+
+btnBrush.addEventListener('click', switcher);
+
+btnFill.addEventListener('click', switcher);
+
+btnSave.addEventListener('click', function(){
+	for (let j = 0; j < pixels.length; j++){
+		saveFile.pixels.push({j: pixels[j].style.background});
+	};
+	localStorage.setItem('session', JSON.stringify(saveFile));
+	console.log(saveFile);
 });
 
-var curColor = 'black';
-var colorPicker = document.getElementById('Color');
-var colorWrap = document.getElementById('ColorWrap');
-	colorWrap.style.background = curColor;
-colorPicker.addEventListener('change', function(){
-	curColor = event.target.value;
-	colorWrap.style.background = curColor;
-});
-
-var btnFill = document.getElementById('Fill');
-var curTool = null;
-	btnFill.addEventListener('click', function(){
+btnLoad.addEventListener('click', function(){
+	loadFile = JSON.parse(localStorage.getItem('session'));
+	for (let j = 0; j < loadFile.length; j++){
 		
-	});
-/* var colors = document.querySelectorAll(".ColorPall"); //Сбор цветов в палитре
-var curColor = '';
-for (let i = 0; i < colors.length; i++){
-  colors[i].addEventListener('click', function(){
-		var curColorBox = document.getElementsByClassName("CurColor"); //Назначение текущего цвета
-    curColor = this.id;
-		curColorBox[0].id = this.id;
-   });
-}; */
+	};
+	console.log(loadFile);
+
+/* 	loadFile.pixels.push(JSON.parse(localStorage.getItem('session')));
+
+	pixWindow.innerHTML = loadFile.pixels[0]; */
+});
